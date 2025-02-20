e62824f7e2b9203b151b633a2fb55f34c7f7cb46
Hyrum Kammerman <kammerman.h@gmail.com>
2025-02-20 11:13:35 -0700

    removed RegisterHandler class, replaced it with method in Server class

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,11 +1,25 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import org.eclipse.jetty.server.Authentication;
+import service.UserService;
 import spark.*;
+import spark.Request;
+
+import static dataaccess.UserDao.BAD_REQUEST_ERR_MSG;
+import static dataaccess.UserDao.USER_TAKEN_ERR_MSG;
 
 public class Server {
-// TODO: create an instance of each handler here, which will store respective service and DAO instances in itself
-    private RegisterHandler regHandler = new RegisterHandler();
+//    TODO: get rid of handler classes and instead implement handlers as functions in this class
+//    TODO: get rid of ClearService and instead implement clear() in other services
+//    TODO: initialize an instance of each service in this class, which will store their respective dao instances
     private ClearHandler clearHandler = new ClearHandler();
+
+    private Gson serializer = new Gson();
+    private String serialize(Object result) {
+        return serializer.toJson(result);
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -13,7 +27,7 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-        Spark.post("/user", regHandler);
+        Spark.post("/user", this::register);
         Spark.delete("/db", clearHandler);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
@@ -21,6 +35,24 @@ public class Server {
 
         Spark.awaitInitialization();
         return Spark.port();
+    }
+
+    private UserService userService = new UserService();
+
+    private Object register(Request request, Response response) throws DataAccessException {
+        RegisterRequest regReq = (RegisterRequest) serializer.fromJson(request.body(), RegisterRequest.class);
+        RegisterResult result = userService.register(regReq);
+        switch (result.message()) {
+            case null: response.status(200);
+            break;
+            case USER_TAKEN_ERR_MSG: response.status(403);
+            break;
+            case BAD_REQUEST_ERR_MSG: response.status(400);
+            break;
+            default: response.status(500);
+        }
+        response.body(serialize(result));
+        return response.body();
     }
 
     public void stop() {
