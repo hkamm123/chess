851f711573af2d67d0da5c676007efe07d048139
Hyrum Kammerman <kammerman.h@gmail.com>
2025-02-24 20:54:48 -0700

    changed implementation in MemoryAuthDao to an Arraylist (was HashMap)

--- server/src/main/java/dataaccess/MemoryAuthDao.java
+++ server/src/main/java/dataaccess/MemoryAuthDao.java
@@ -3,43 +3,54 @@ package dataaccess;
 import model.AuthData;
 import server.ClearResult;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 public class MemoryAuthDao implements AuthDao {
-    private HashMap<String, String> auths = new HashMap<>();
+//    private HashMap<String, String> auths = new HashMap<>();
+    private Collection<AuthData> auths = new ArrayList<>();
 
     @Override
     public AuthData createAuth(String username) {
         AuthData authData = new AuthData(UUID.randomUUID().toString(), username);
-        auths.put(authData.username(), authData.authToken());
+        auths.add(authData);
         return authData;
     }
 
     @Override
     public String getAuthToken(String username) {
-        return auths.get(username);
+        for (AuthData data : auths) {
+            if (data.username().equals(username)) {
+                return data.authToken();
+            }
+        }
+        return null;
     }
 
     @Override
     public boolean containsToken(String authToken) {
-        return auths.containsValue(authToken);
+        for (AuthData data : auths) {
+            if (data.authToken().equals(authToken)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public boolean deleteAuth(String authToken) {
-        if (this.containsToken(authToken)) {
-            // removes the key value pair from the map by only passing in the value
-            auths.values().remove(authToken);
-            return true;
+        Iterator<AuthData> itr = auths.iterator();
+        while (itr.hasNext()) {
+            AuthData data = itr.next();
+            if (data.authToken().equals(authToken)) {
+                itr.remove();
+                return true;
+            }
         }
         return false;
     }
 
     @Override
     public void clear() {
-        this.auths = new HashMap<>();
+        this.auths = new ArrayList<>();
     }
 }
