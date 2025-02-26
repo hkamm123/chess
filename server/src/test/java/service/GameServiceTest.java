4411a581654ceb99d069d658f43579da251b5ec8
Hyrum Kammerman <kammerman.h@gmail.com>
2025-02-25 21:35:25 -0700

    implemented tests for GameService.listGames

--- server/src/test/java/service/GameServiceTest.java
+++ server/src/test/java/service/GameServiceTest.java
@@ -7,8 +7,9 @@ import dataaccess.MemoryGameDao;
 import model.AuthData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import server.ListResult;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameServiceTest {
     GameDao testGameDao;
@@ -20,6 +21,27 @@ public class GameServiceTest {
         testGameDao = new MemoryGameDao();
         testAuthDao = new MemoryAuthDao();
         testGameService = new GameService(testGameDao, testAuthDao);
+    }
+
+    @Test
+    public void successListGames() {
+        testGameDao.createGame("sample game");
+        testGameDao.createGame("second sample game");
+        AuthData auth = testAuthDao.createAuth("username");
+        assertTrue(testAuthDao.containsToken(auth.authToken()), "auth token not created in auth dao");
+
+        ListResult testResult = testGameService.listGames(auth.authToken());
+        assertEquals(2, testResult.games().size(),
+                "expected 2 games, received: " + testResult.games().size());
+        assertNull(testResult.message(), "message received was not null");
+    }
+
+    @Test
+    public void listGamesFailWhenBadAuthGiven() {
+        testGameDao.createGame("a sample game");
+        ListResult testResult = testGameService.listGames("a bad auth token");
+        assertNull(testResult.games(), "result games is not null");
+        assertNotNull(testResult.message(), "result message is null");
     }
 
     @Test
