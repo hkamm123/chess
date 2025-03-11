637892545e8d461192aab9b203a2b5825020b43b
Hyrum Kammerman <kammerman.h@gmail.com>
2025-03-10 19:55:54 -0600

    removed todo comment

--- server/src/test/java/dataaccess/MySQLUserDaoTest.java
+++ server/src/test/java/dataaccess/MySQLUserDaoTest.java
@@ -66,7 +66,7 @@ public class MySQLUserDaoTest {
     }
 
     @Test
-    public void successCreateUser() { // TODO: write this test
+    public void successCreateUser() {
         UserData userToCreate = new UserData(
                 "newUser", "newHash", "testEmail"
         );
