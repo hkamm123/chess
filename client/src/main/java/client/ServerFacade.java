b3b389cb45d29105e845c2743a9c1b21e3b94efd
Hyrum Kammerman <kammerman.h@gmail.com>
2025-03-18 10:26:00 -0600

    skeleton for ServerFacade and ClientCommunicator

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,10 +1,48 @@
 package client;
 
+import com.google.gson.Gson;
+import server.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
 public class ServerFacade {
-    private final String serverUrl;
+    private final ClientCommunicator clientCommunicator;
 
     public ServerFacade(String serverUrl) {
-        this.serverUrl = serverUrl;
+        this.clientCommunicator = new ClientCommunicator(serverUrl);
     }
-    // TODO: implement all methods corresponding to endpts in Server.java
+
+    public RegisterResult register(RegisterRequest req) {
+        // TODO: implement
+    }
+
+    public LogoutResult clear() { // LogoutResult is reused for this because ClearResult would be same
+        // TODO: implement
+    }
+
+    public RegisterResult login(LoginRequest req) {
+        // TODO: implement
+    }
+
+    public LogoutResult logout() {
+        // TODO: implement
+    }
+
+    public ListResult listGames() {
+        // TODO: implement
+    }
+
+    public CreateResult createGame() {
+        // TODO: implement
+    }
+
+    public JoinResult joinGame() {
+        // TODO: implement
+    }
 }
