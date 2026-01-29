fac8962edba4b4111b3bef31f3692826e43d6a57
Hyrum Kammerman <kammerman.h@gmail.com>
2026-01-29 13:43:38 -0700

    stub out register endpt

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,27 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
+import io.javalin.http.Context;
+import org.jetbrains.annotations.NotNull;
+import server.request.*;
 
 public class Server {
 
     private final Javalin javalin;
+    private final Gson gson;
 
     public Server() {
+        gson = new Gson();
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
+        javalin.post("/user", this::registrationHandler);
+    }
 
+    private void registrationHandler(@NotNull Context context) {
+        RegisterRequest request = gson.fromJson(context.body(), RegisterRequest.class);
+        // TODO: pass request to Service, send back response
     }
 
     public int run(int desiredPort) {
