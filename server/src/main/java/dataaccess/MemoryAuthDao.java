f23367b7880207530a93297b2be721cc328e8d83
Hyrum Kammerman <kammerman.h@gmail.com>
2026-01-30 13:20:52 -0700

    add getAuth and deleteAuth to AuthDao interface

--- server/src/main/java/dataaccess/MemoryAuthDao.java
+++ server/src/main/java/dataaccess/MemoryAuthDao.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MemoryAuthDao implements AuthDao{
-    private final Map<String, String> sessions;
+    private final Map<String, AuthData> sessions;
 
     public MemoryAuthDao() {
         sessions = new HashMap<>();
@@ -14,6 +14,16 @@ public class MemoryAuthDao implements AuthDao{
 
     @Override
     public void createAuth(AuthData authData) throws DataAccessException {
-        sessions.put(authData.authToken(), authData.username());
+        sessions.put(authData.authToken(), authData);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return sessions.get(authToken);
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        sessions.remove(authToken);
     }
 }
