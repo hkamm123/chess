6b164cf35d1e2c5b0b4ecaa8a916d422e141f431
Hyrum Kammerman <kammerman.h@gmail.com>
2026-02-03 10:01:33 -0700

    implement service for create game

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -4,6 +4,8 @@ import dataaccess.AuthDao;
 import dataaccess.DataAccessException;
 import dataaccess.GameDao;
 import model.GameData;
+import server.request.CreateRequest;
+import server.result.CreateResult;
 import server.result.ListGamesResult;
 
 import java.util.List;
@@ -24,6 +26,18 @@ public class GameService {
             }
             List<GameData> games = gameDao.getGames();
             return new ListGamesResult(games);
+        } catch (DataAccessException ex) {
+            throw new ServiceException(ServiceException.ServiceExceptionType.SERVER_ERROR);
+        }
+    }
+
+    public CreateResult createGame(String authToken, CreateRequest request) throws ServiceException {
+        try {
+            if (authDao.getAuth(authToken) == null) {
+                throw new ServiceException(ServiceException.ServiceExceptionType.UNAUTHORIZED);
+            }
+            int gameID = gameDao.createGame(request.gameName());
+            return new CreateResult(gameID);
         } catch (DataAccessException ex) {
             throw new ServiceException(ServiceException.ServiceExceptionType.SERVER_ERROR);
         }
